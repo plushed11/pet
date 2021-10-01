c5ae64e23b180d253f2831ee820d1b8148c13f01
plushed11 <91710016+plushed11@users.noreply.github.com>
2021-09-30 20:20:40 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Heart)
+    soundExpression.giggle.playUntilDone()
+    basic.showIcon(IconNames.Heart)
+})
+input.onButtonPressed(Button.B, function () {
+    soundExpression.yawn.playUntilDone()
+    basic.showIcon(IconNames.Asleep)
+    soundExpression.giggle.playUntilDone()
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Confused)
+    soundExpression.sad.playUntilDone()
+    basic.showIcon(IconNames.Sad)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Happy)
+    soundExpression.twinkle.playUntilDone()
+    basic.showIcon(IconNames.Sad)
+})
+basic.showIcon(IconNames.Happy)
